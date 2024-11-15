f1780a63beba5bf792ca091e5ec6b51404484fcb
Arvind <arvindsharma50480@gmail.com>
2024-11-16 01:00:40 +0530

    First commit

---
+++ firstScript.js
@@ -0,0 +1,7 @@
+console.log('hello');
+/*
+ the core difference bw TS and JS is that TS is compile time language and JS is runtime language
+so at compile time only TS will point errors unlike JS as here we are mentioning the type
+ */
+var age = 30;
+var check = "Arvind";
---
+++ firstScript.ts
@@ -0,0 +1,13 @@
+console.log('hello');
+/* 
+ the core difference bw TS and JS is that TS is compile time language and JS is runtime language
+so at compile time only TS will point errors unlike JS as here we are mentioning the type. So TS is the static
+type language 
+ 
+TS syntax
+
+let age: number =30;      
+let check: string="Arvind";
+
+*/
+
